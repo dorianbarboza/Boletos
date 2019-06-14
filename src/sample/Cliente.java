7043d95c47116718b5c9a494f999a3c1426a9461
dorianbarboza <dorianbarboza@gmail.com>
2019-06-14 02:19:09 -0500

    8. Avance

--- src/sample/Cliente.java
+++ src/sample/Cliente.java
@@ -1,18 +1,60 @@
 package sample;
 
-public class Cliente {
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
 
+public class Cliente {
+    /*
     private int clienteId;
     private String nombre;
     private String apellidos;
     private String direccion;
 
-    public Cliente(int clienteId, String nombre, String apellidos, String direccion) {
+     */
+    private IntegerProperty clienteId;
+    private StringProperty nombre;
+    private  StringProperty apellidos;
+    private StringProperty direccion;
+
+    public Cliente(IntegerProperty clienteId, StringProperty nombre, StringProperty apellidos, StringProperty direccion) {
         this.clienteId = clienteId;
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.direccion = direccion;
     }
+
+    public int getClienteId() {
+        return clienteId.get();
+    }
+
+    public IntegerProperty clienteIdProperty() {
+        return clienteId;
+    }
+
+    public String getNombre() {
+        return nombre.get();
+    }
+
+    public StringProperty nombreProperty() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos.get();
+    }
+
+    public StringProperty apellidosProperty() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion.get();
+    }
+
+    public StringProperty direccionProperty() {
+        return direccion;
+    }
+    /*
 
     public String getNombre() {
         return nombre;
@@ -37,6 +79,8 @@ public class Cliente {
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
+
+     */
 
     @Override
     public String toString() {
